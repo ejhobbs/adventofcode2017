740302c06775208c8c815d89487ebf3345f94971
Edward Hobbs <eh362@kent.ac.uk>
2017-12-03 21:20:10 +0000

    refactoring to make things prettier and less clashy

--- lib/adventofcode2017.ex
+++ lib/adventofcode2017.ex
@@ -1,12 +1,12 @@
 defmodule Adventofcode2017 do
   def solve(day,year,mod,:first) do
     {:ok, input} = AdventOfCodeHelper.get_input(year,day)
-    mod.solve(input)
+    Module.concat([mod,PartOne]).solve(input)
       |> IO.write
   end
   def solve(day,year,mod,:second) do
     {:ok, input} = AdventOfCodeHelper.get_input(year,day)
-    mod.solve2(input)
+    Module.concat([mod,PartTwo]).solve(input)
       |> IO.write
   end
 end
--- lib/day_one.ex
+++ lib/day_one.ex
@@ -1,37 +1,43 @@
 defmodule DayOne do
-    def solve([],total) do
-        total
-    end
-    
-    def solve([_x],total) do 
-        total
-    end
-
-    def solve([x,y],total) do
-        total + compare(x,y)
-    end
-
-    def solve([x,y|xs],total) do
-        solve([y|xs], total + compare(x,y))
-    end
-
-
-
-    def solve(input) do
-        input = String.split(input,"",trim: true)
-            |> Enum.map(&(String.to_integer(&1)))
-        if Enum.count(input) > 2 do
-            solve(input,compare(Enum.at(input,0),Enum.at(input,-1)))
-        else
-            solve(input,0)
-        end
-    end
-
-    defp compare(a,a) do
+    def compare(a,a) do
         a
     end
 
-    defp compare(_,_) do
+    def compare(_,_) do
         0
+    end
+
+    defmodule PartOne do
+        def solve([],total) do
+            total
+        end
+        
+        def solve([_x],total) do 
+            total
+        end
+
+        def solve([x,y],total) do
+            total + DayOne.compare(x,y)
+        end
+
+        def solve([x,y|xs],total) do
+            solve([y|xs], total + DayOne.compare(x,y))
+        end
+
+        def solve(input) do
+            input = String.split(input,"",trim: true)
+                |> Enum.map(&(String.to_integer(&1)))
+            if Enum.count(input) > 2 do
+                solve(input,DayOne.compare(Enum.at(input,0),Enum.at(input,-1)))
+            else
+                solve(input,0)
+            end
+        end
+    end
+
+    defmodule PartTwo do
+        def solve(input) do 
+            0
+        end
     end
 end
